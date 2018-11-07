ecd55e975b0c9f906f3f7969bb51bdb610abf2b4
张俊亚 <voguedi@gmail.com>
2018-11-07 12:03:01 +0800

    移除：缓存部分 新增：后台定时工作器

---
+++ src/Voguedi.Utils/Voguedi/BackgroundWorkers/BackgroundWorker.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace Voguedi.BackgroundWorkers
+{
+    class BackgroundWorker : IBackgroundWorker
+    {
+        #region Private Class
+
+        class BackgroundWorkerContext
+        {
+            #region Public Properties
+
+            public string Key { get; set; }
+
+            public Action Action { get; set; }
+
+            public Timer Timer { get; set; }
+
+            public int DueTime { get; set; }
+
+            public int Period { get; set; }
+
+            public bool Started { get; set; }
+
+            #region Private Fields
+
+            readonly ILogger logger;
+            readonly object syncLock = new object();
+            readonly ConcurrentDictionary<string, BackgroundWorkerContext> contextMapping = new ConcurrentDictionary<string, BackgroundWorkerContext>();
+
+            #endregion
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        readonly ILogger logger;
+        readonly object syncLock = new object();
+        readonly ConcurrentDictionary<string, BackgroundWorkerContext> contextMapping = new ConcurrentDictionary<string, BackgroundWorkerContext>();
+
+        #endregion
+
+        #region Ctors
+
+        public BackgroundWorker(ILogger<BackgroundWorker> logger) => this.logger = logger;
+
+        #endregion
+
+        #region Private Methods
+
+        void Callback(object state)
+        {
+            var key = state.ToString();
+
+            if (contextMapping.TryGetValue(key, out var context))
+            {
+                try
+                {
+                    if (context.Started)
+                    {
+                        context.Timer.Change(Timeout.Infinite, Timeout.Infinite);
+                        context.Action?.Invoke();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"后台工作器当前状态异常！ [Key = {context.Key}, DueTime = {context.DueTime}, Period = {context.Period}]");
+                }
+                finally
+                {
+                    try
+                    {
+                        if (context.Started)
+                            context.Timer.Change(context.Period, context.Period);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"后台工作器当前状态异常！ [Key = {context.Key}, DueTime = {context.DueTime}, Period = {context.Period}]");
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region IBackgroundWorker
+
+        public void Start(string key, Action action, int dueTime, int period)
+        {
+            lock (syncLock)
+            {
+                if (!contextMapping.ContainsKey(key))
+                {
+                    var timer = new Timer(Callback, key, Timeout.Infinite, Timeout.Infinite);
+                    contextMapping.TryAdd(
+                        key,
+                        new BackgroundWorkerContext
+                        {
+                            Action = action,
+                            DueTime = dueTime,
+                            Key = key,
+                            Period = period,
+                            Started = true,
+                            Timer = timer
+                        });
+                    timer.Change(dueTime, period);
+                }
+            }
+        }
+
+        public void Stop(string key)
+        {
+            lock (syncLock)
+            {
+                if (contextMapping.TryGetValue(key, out var context))
+                {
+                    context.Started = false;
+                    context.Timer.Dispose();
+                    contextMapping.TryRemove(key);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Voguedi.Utils/Voguedi/BackgroundWorkers/IBackgroundWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Voguedi.DependencyInjection;
+
+namespace Voguedi.BackgroundWorkers
+{
+    public interface IBackgroundWorker : ISingletonDependency
+    {
+        #region Methods
+
+        void Start(string key, Action action, int dueTime, int period);
+
+        void Stop(string key);
+
+        #endregion
+    }
+}
